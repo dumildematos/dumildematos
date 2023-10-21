35f0144a6938e641b9c00acd622ec82889270223
Dumilde Matos <dumildejosematos@gmail.com>
2023-10-21 22:07:25 +0100

    show username

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h2>
  
-  Hi anonymous user! Welcome to my GitHub page.
+  
 
 </h2>
 
@@ -58,15 +58,6 @@
 
 ### 📔 Latest Blog posts
 <ul>
-  <li>
-    <a href="https:&#x2F;&#x2F;dumildematos.medium.com&#x2F;what-you-should-know-about-angular-75cd74786fd7?source&#x3D;rss-a700ea04d69------2" target="_blank">What you should know about Angular.</a>
-  </li>
-  <li>
-    <a href="https:&#x2F;&#x2F;dumildematos.medium.com&#x2F;o-que-%C3%A9-um-servi%C3%A7o-no-angular-e-por-que-us%C3%A1-lo-2c8b441618cf?source&#x3D;rss-a700ea04d69------2" target="_blank">O que é um serviço no Angular e por que usá-lo?</a>
-  </li>
-  <li>
-    <a href="https:&#x2F;&#x2F;dumildematos.medium.com&#x2F;o-que-%C3%A9-gitflow-workflow-e-porqu%C3%AA-usar-b45871d6412e?source&#x3D;rss-a700ea04d69------2" target="_blank">O que é Gitflow Workflow e porquê usar ?</a>
-  </li>
 </ul>
 
 <!-- BLOG-POST-LIST:END -->
@@ -81,5 +72,5 @@
 </p>
 
 ------------
-<p align="center">This <i>README</i> file is generated <b>every 3 hours</b>!</br>Last refresh: <br /></p>
+<p align="center">This <i>README</i> file is generated <b>every 3 hours</b>!</br>Last refresh: Saturday, 21 October at 22:06 WEST<br /></p>
 <p align="center"><img src="https://github.com/dumildematos/dumildematos/workflows/README%20build/badge.svg" /> <img alt="Stars" src="https://img.shields.io/github/stars/dumildematos/dumildematos?style=flat-square&labelColor=343b41"/> <img alt="Forks" src="https://img.shields.io/github/forks/dumildematos/dumildematos?style=flat-square&labelColor=343b41"/></p>
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const Mustache = require('mustache');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const puppeteerService = require('./services/puppeteer.service');
-const username = process.env.GITHUB_USER;
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
+const githubApiUrl = 'https://api.github.com/user';
+const githubToken = process.env.GITHUB_TOKEN; // Your GitHub token
 let DATA = {
   refresh_date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
@@ -18,6 +19,28 @@ let DATA = {
   }),
 };
 
+
+// Fetch the user's information from the GitHub API
+async function fetchUserInfo() {
+  try {
+    const response = await fetch(githubApiUrl, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+        'User-Agent': 'GitHub-Hello-App',
+      },
+    });
+
+    if (response.status === 200) {
+      const user = await response.json();
+      return user.login; // Return the GitHub username
+    } else {
+      return null; // User not logged in or other error
+    }
+  } catch (error) {
+    console.error('Error fetching user info:', error);
+    return null;
+  }
+}
 async function setWeatherInformation() {
   await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
@@ -47,17 +70,27 @@ async function setInstagramPosts() {
   DATA.img3 = instagramImages[2];
 }
 
+async function getGreeting(){
+  const username = await fetchUserInfo();
+  console.log(username);
+  if (username) {
+    return `Hi ${username}! Welcome to my GitHub page.`;
+  } else {
+    return 'Hi anonymous user! Welcome to my GitHub.';
+  }
+}
+
 async function setMediumPosts(){
   const rssFeedUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@dumildematos';
 
- fetch(rssFeedUrl)
+ await fetch(rssFeedUrl)
   .then((response) => response.json())
   .then((data) => {
     const posts = data.items.slice(0,3);
-const greeting = generateGreeting(username);
     const template = fs.readFileSync('main.mustache', 'utf8');
+    console.log({greeting: getGreeting(), items: posts })
 
-    const rendered = Mustache.render(template, {greeting, items: posts });
+    const rendered = Mustache.render(template, {greeting: getGreeting(), items: posts });
 
     fs.writeFileSync('README.md', rendered);
   })
@@ -75,13 +108,7 @@ async function generateReadMe() {
     fs.writeFileSync('README.md', output);
   });
 }
-function generateGreeting(username) {
-  if (username) {
-    return `Hi ${username}! Welcome to my GitHub page.`;
-  } else {
-    return 'Hi anonymous user! Welcome to my GitHub page.';
-  }
-}
+
 async function action() {
   /**
    * Fetch Weather
